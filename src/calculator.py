70bda752b57dcf488750f8e5408436bb6d5943cc
v-s-v-i-s-h-w-a-s <vishwasvshanthkumar@gmail.com>
2025-09-14 13:47:25 +0530

    Base Files

---
+++ src/calculator.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    # Addition function
+    
+
+def subtract(a, b):
+    # Substraction function
+    
+
---
+++ tests/test_calculator.py
@@ -0,0 +1,7 @@
+from src import calculator
+
+def test_add():
+    assert calculator.add(2, 3) == 5
+
+def test_subtract():
+    assert calculator.subtract(5, 2) == 3
