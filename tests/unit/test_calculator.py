940c95bcabbc867fa0e85426b574afb979b571dd
v-s-v-i-s-h-w-a-s <vishwasvshanthkumar@gmail.com>
2025-09-14 13:57:29 +0530

    Base Files

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -1,7 +1,31 @@
-from src import calculator
+"""
+Unit Tests for Calculator
+Students start with 2 passing tests, then add more
+"""
+import pytest
+from src.calculator import add, subtract
 
-def test_add():
-    assert calculator.add(2, 3) == 5
+class TestBasicOperations:
+    """Test basic arithmetic operations"""
+    
+    def test_add_positive_numbers(self):
+        """Test adding positive numbers"""
+        assert add(2, 3) == 5
+        assert add(10, 15) == 25
+    
+    def test_add_negative_numbers(self):
+        """Test adding negative numbers"""
+        assert add(-1, -1) == -2
+        assert add(-5, 3) == -2
+    
+    def test_subtract_positive_numbers(self):
+        """Test subtracting positive numbers"""
+        assert subtract(5, 3) == 2
+        assert subtract(10, 4) == 6
+    
+    def test_subtract_negative_numbers(self):
+        """Test subtracting negative numbers"""
+        assert subtract(-1, -1) == 0
+        assert subtract(-5, -3) == -2
 
-def test_subtract():
-    assert calculator.subtract(5, 2) == 3
+# TODO: Students will add TestMultiplyDivide class
